46004034ca01fb2fa62f6b14405f88c952dc9c01
vinnidias <vinicius.3991@gmail.com>
2022-07-07 20:26:17 -0300

    first exec db migration

---
+++ prisma/migrations/20220707231343_create_table_users/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
